1b2c2d6c7f17057dd8f96e6d7347290304de5c2c
jffersn <32963293+jffersn@users.noreply.github.com>
2019-09-21 17:32:26 -0400

    fixed page onload
    JS script now runs onload anysite

--- background.js
+++ background.js
@@ -1,3 +1,11 @@
 window.onload=function(){
-    console.log("page load!");
+    console.log("page loaded!");
+
+    var elements = document.querySelectorAll( 'body *' );//get all elements as an array
+    console.log(elements);
+    for(var i = 0; i < elements.length; i++){
+        console.log(elements[i].innerHTML);
+    }
+    
 }
+//* and i -> me and *
